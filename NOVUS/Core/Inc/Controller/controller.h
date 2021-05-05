6b9bcc5a0e637d6763c71fc4ee5ecd7e01972025
junsuk718 <junsuk718@naver.com>
2021-05-05 17:52:13 +0900

    .
    .

--- NOVUS/Core/Inc/Controller/controller.h
+++ NOVUS/Core/Inc/Controller/controller.h
@@ -1,4 +1,32 @@
 #pragma once
+/**
+  ******************************************************************************
+  * @file           : controller.h
+  * @brief          : Header for controller.c file.
+  *                   This file contains the common defines of the application.
+  * @author NOVUS Graduation Project Team
+  ******************************************************************************
+  * @details
+  *
+  * Setting & Management
+  * 
+  * as5147 SETTING
+  *     as5147_Init - as5147 initialize
+  *		as5147_setZeroPosition - set angle to zeroposition
+  *		as5147_readPosition - read angle from as5147
+  *
+  * as5147 R/W
+  * 	registerRead - read register value
+  * 	registerWrite - write register value
+  * 	packCommandFrame - 
+  * 	calcParity
+  * 
+  * angle caculator
+  * 	calcRPM - calculate RPM
+  *
+  ******************************************************************************
+  */
+
   /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __CONTROLLER_H
 #define __CONTROLLER_H
@@ -9,11 +37,15 @@
 #define PWM_MIN 1190
 #define PWM_MAX 1850
 
+/**
+ * @brief RC 비행 모드에 사용될 모드 종류들
+ * 
+ */
 typedef enum control_mode{
-  CUT_OFF = 0,
-  ARM,
-  NON_MOMENT,
-  MOMENT
+  CUT_OFF = 0,  /*!< all process stop */
+  ARM,          /*!< arming motor(set motor to MIN speed )*/
+  NON_MOMENT,   /*!< there is no moment control in this mode*/
+  MOMENT        /*!< there is*/
 }MODE;
 
 float pre_error;
